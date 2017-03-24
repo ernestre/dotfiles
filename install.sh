685d4995fa92e5a619a52e3994e11ef1fa6612da
Ernest Rekel <ernest.rekel@gmail.com>
2017-03-24 10:41:43 +0200

    Add zsh config

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles          # dotfiles directory
 olddir=~/dotfiles_old   # old dotfiles backup directory
-files="vimrc tmux.conf ctags gitconfig gitignore_global" # list of files/folders to symlink in homedir
+files="vimrc tmux.conf ctags gitconfig gitignore_global zshrc" # list of files/folders to symlink in homedir
 
 ##########
 
