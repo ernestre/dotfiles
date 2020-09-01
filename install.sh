4f856d9200722adc31d91504b8a65ad5e99a2beb
Ernest Rekel <ernest.rekel@gmail.com>
2020-09-01 23:11:42 +0300

    Refactor installer script

--- install.sh
+++ install.sh
@@ -1,58 +1,41 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+scriptSource=$(dirname "${BASH_SOURCE[0]}")
 
-dir=~/dotfiles          # dotfiles directory
-olddir=~/dotfiles_old   # old dotfiles backup directory
-files="vimrc tmux.conf ctags gitconfig gitignore_global zshrc" # list of files/folders to symlink in homedir
+bash $scriptSource/installers/dependencies.sh
+bash $scriptSource/installers/bat.sh
+bash $scriptSource/installers/ctop.sh
+bash $scriptSource/installers/go.sh
+bash $scriptSource/installers/nodejs.sh
 
-##########
+bash $scriptSource/dependencies/node.sh
+bash $scriptSource/dependencies/php.sh
 
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+# ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -s --skip-chsh --unattended
 
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+[ -d ~/.vim ] || mkdir -v ~/.vim
 
-echo "Moving any existing dotfiles from ~ to $olddir"
+dir=dotfiles
+ln -sv ~/$dir/vim/plugin/ ~/.vim/plugin
+ln -sv ~/$dir/vim/ftplugin/ ~/.vim/ftplugin
+
+files=".ctags .gitconfig .gitignore_global .tmux.conf .vimrc .zshrc .config" # list of files/folders to symlink in homedir
 for file in $files; do
-    if [ -f ~/.$file  ]; then
-        mv ~/.$file $olddir
-        echo $file "moved to" $olddir
+    if [ -f ~/$file ]
+    then
+        rm -rfv ~/$file
     fi
-done
-echo "...done"
 
-echo "Creating symlinks"
-for file in $files; do
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/.$file ~/.$file
+    ln -sv ~/$dir/$file ~/$file
 done
-echo "...done"
 
-echo "Installing TPM"
+# Tmux plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "...done"
-
-echo "Installing Tmux plugins"
 ~/.tmux/plugins/tpm/bin/install_plugins
-echo "...done"
 
-echo "Installing FZF"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-echo "...done"
-
-echo "Installing Plug"
+# vimplug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "...Done"
 
-echo "Installing Vim plugins"
-vim +PlugInstall +qall
-echo "...Done"
+# Install vim plugins
+vim --headless +PlugInstall +qall
