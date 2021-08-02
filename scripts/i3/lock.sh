1ce744945fd7e4d76187559cfa15129b5204a33e
Ernest Rekel <ernest.rekel@gmail.com>
2021-08-02 11:21:38 +0300

    Change wallpaper

--- scripts/i3/lock.sh
+++ scripts/i3/lock.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
-i3lock -c 000000; systemctl suspend
+i3lock -i ~/dotfiles/wallpapers/main.png ; # systemctl suspend
