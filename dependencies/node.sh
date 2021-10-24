a2bf62727d1ab9eb636489a123911722b50bd04b
Ernest Rekel <ernest.rekel@gmail.com>
2021-10-24 14:50:28 +0300

    Add missing lsp dependencies

--- dependencies/node.sh
+++ dependencies/node.sh
@@ -5,5 +5,9 @@ IFS=$'\n\t'
 sudo npm install -g \
     instant-markdown-d \
     git-split-diffs \
-    intelephense
+    intelephense \
+    typescript \
+    typescript-language-server \
+    vscode-langservers-extracted \
+    yaml-language-server
 
