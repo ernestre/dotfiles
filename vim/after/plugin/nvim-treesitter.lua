77dfaa9b491dc2f5c4159140fa37824521930387
Ernest Rekel <ernest.rekel@gmail.com>
2021-12-29 14:36:47 +0200

    Add hlc syntax highlight

--- vim/after/plugin/nvim-treesitter.lua
+++ vim/after/plugin/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"json", "javascript", "jsdoc", "typescript", "go", "php", "html"},
+  ensure_installed = {"json", "javascript", "jsdoc", "typescript", "go", "php", "html", "hcl"},
   highlight = {
     enable = true,
   },
