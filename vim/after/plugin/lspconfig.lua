c96d4858ecbae390bd3fdd14165b5ba5ab69f7a5
Ernest Rekel <ernest.rekel@gmail.com>
2022-02-02 09:04:23 +0200

    Move lua configurations to separate files

---
+++ vim/after/plugin/lspconfig.lua
@@ -0,0 +1,7 @@
+require'lspconfig'.gopls.setup{}
+require'lspconfig'.intelephense.setup{}
+require'lspconfig'.jsonls.setup{}
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.tsserver.setup{}
+require'lspconfig'.yamlls.setup{}
+require'lspconfig'.terraformls.setup{}
