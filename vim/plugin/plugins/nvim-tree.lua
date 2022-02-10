128b298cb87a8f975eecef8f199b596274b80ef5
Ernest Rekel <ernest.rekel@gmail.com>
2022-02-10 12:04:11 +0200

    Add some additional nvim tree configs

--- vim/plugin/plugins/nvim-tree.lua
+++ vim/plugin/plugins/nvim-tree.lua
@@ -1,4 +1,15 @@
 local g = vim.g
+
+g.nvim_tree_git_hl = 1
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
+
+g.nvim_tree_show_icons = {
+   folders = 1,
+   files = 1,
+   git = 1,
+}
+
 g.nvim_tree_icons = {
    default = "",
    symlink = "",
@@ -10,14 +21,6 @@ g.nvim_tree_icons = {
       unmerged = "",
       unstaged = "✗",
       untracked = "★",
-   },
-   folder = {
-      default = "",
-      open = "",
-      empty = "",
-      empty_open = "",
-      symlink = "",
-      symlink_open = "",
    },
 }
 
