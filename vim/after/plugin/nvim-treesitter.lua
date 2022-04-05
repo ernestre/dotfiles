9fdba8ee401ce4ca68f40c851a06dc1eefacd06e
Ernest Rekel <ernest.rekel@gmail.com>
2022-04-05 10:40:29 +0300

    Remove html from treesitter

--- vim/after/plugin/nvim-treesitter.lua
+++ vim/after/plugin/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"json", "javascript", "jsdoc", "typescript", "go", "php", "html", "hcl"},
+  ensure_installed = {"json", "javascript", "jsdoc", "typescript", "go", "php", "hcl"},
   highlight = {
     enable = true,
   },
