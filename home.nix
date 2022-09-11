eb9e0d9fc40940a265c7256c1b288638cffd6749
Ernest Rekel <ernest.rekel@gmail.com>
2022-09-11 13:55:08 -0700

    Add zsh

--- home.nix
+++ home.nix
@@ -31,13 +31,13 @@ in {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.sessionVariables = {
-    # EDITOR = "nvim";
-    LANG = "en_US.UTF-8";
-    LC_ALL = "en_US.UTF-8";
-    LC_CTYPE = "en_US.UTF-8";
-    PULUMI_K8S_SUPPRESS_HELM_HOOK_WARNINGS = "true";
-  };
+  # home.sessionVariables = {
+  #   EDITOR = "nvim";
+  #   LANG = "en_US.UTF-8";
+  #   LC_ALL = "en_US.UTF-8";
+  #   LC_CTYPE = "en_US.UTF-8";
+  #   PULUMI_K8S_SUPPRESS_HELM_HOOK_WARNINGS = "true";
+  # };
 
   home.packages = with pkgs; [
     git
@@ -47,7 +47,6 @@ in {
     fd
     ripgrep
     tree
-    zsh
     go
   ];
 
@@ -147,5 +146,38 @@ in {
           plugins.tree-sitter-nix
         ]))
     ];
+  };
+
+  programs.zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      enableSyntaxHighlighting = true;
+
+      oh-my-zsh = {
+          enable = true;
+          plugins = [ "git" ];
+          theme = "cloud";
+      };
+
+      shellAliases = {
+          cat = "bat --paging=never --theme='base16'";
+          fetch = "git fetch --all --jobs=4 --progress --prune";
+          pull = "git pull --autostash --ff --no-rebase --squash --summary origin";
+          rebase = "git rebase --autostash --merge --stat";
+          ssh = "TERM='xterm-256color' ssh";
+          gs = "git status";
+          gac = "git add . && git commit -v";
+          guc = "git add -u && git commit -v";
+          gaca = "git add . && git commit -v --amend";
+          gacan = "gaca --no-edit";
+      };
+
+      sessionVariables = {
+          EDITOR = "nvim";
+          LANG = "en_US.UTF-8";
+          LC_ALL = "en_US.UTF-8";
+          LC_CTYPE = "en_US.UTF-8";
+      };
   };
 }
