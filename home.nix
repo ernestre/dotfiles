f0b8bbaff7f84a727c7e22f45aceab50caf0d0b9
Ernest Rekel <ernest.rekel@gmail.com>
2022-09-18 08:35:52 -0700

    Migrate zsh

--- home.nix
+++ home.nix
@@ -2,32 +2,14 @@
 
 let
   vimsettings = import ./neovim.nix;
+  zshSettings = import ./zsh.nix;
 in {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "erre";
   home.homeDirectory = "/home/erre";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "22.05";
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  # home.sessionVariables = {
-  #   EDITOR = "nvim";
-  #   LANG = "en_US.UTF-8";
-  #   LC_ALL = "en_US.UTF-8";
-  #   LC_CTYPE = "en_US.UTF-8";
-  #   PULUMI_K8S_SUPPRESS_HELM_HOOK_WARNINGS = "true";
-  # };
 
   home.packages = with pkgs; [
     git
@@ -39,6 +21,7 @@ in {
     tree
     go
     gcc
+    xclip
   ];
 
   programs.bottom.enable = true;
@@ -64,40 +47,8 @@ in {
     };
   };
 
-
   programs.neovim = vimsettings pkgs;
   home.file.".vimrc".source = ./.vimrc;
 
-  programs.zsh = {
-      enable = true;
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      enableSyntaxHighlighting = true;
-
-      oh-my-zsh = {
-          enable = true;
-          plugins = [ "git" ];
-          theme = "cloud";
-      };
-
-      shellAliases = {
-          cat = "bat --paging=never --theme='base16'";
-          fetch = "git fetch --all --jobs=4 --progress --prune";
-          pull = "git pull --autostash --ff --no-rebase --squash --summary origin";
-          rebase = "git rebase --autostash --merge --stat";
-          ssh = "TERM='xterm-256color' ssh";
-          gs = "git status";
-          gac = "git add . && git commit -v";
-          guc = "git add -u && git commit -v";
-          gaca = "git add . && git commit -v --amend";
-          gacan = "gaca --no-edit";
-      };
-
-      sessionVariables = {
-          EDITOR = "nvim";
-          LANG = "en_US.UTF-8";
-          LC_ALL = "en_US.UTF-8";
-          LC_CTYPE = "en_US.UTF-8";
-      };
-  };
+  programs.zsh = zshSettings pkgs;
 }
---
+++ zsh.nix
@@ -0,0 +1,52 @@
+{  pkgs, lib, ...  }:
+{
+  enable = true;
+  enableAutosuggestions = true;
+  enableCompletion = true;
+  enableSyntaxHighlighting = true;
+
+  oh-my-zsh = {
+      enable = true;
+      plugins = [
+          "git"
+          "docker"
+          "docker-compose"
+          "wd"
+          "pass"
+          "kubectl"
+          "helm"
+      ];
+      theme = "cloud";
+  };
+
+  shellAliases = {
+      cat = "bat --paging=never --theme='base16'";
+      fetch = "git fetch --all --jobs=4 --progress --prune";
+      pull = "git pull --autostash --ff --no-rebase --squash --summary origin";
+      rebase = "git rebase --autostash --merge --stat";
+      ssh = "TERM='xterm-256color' ssh";
+      gs = "git status";
+      gac = "git add . && git commit -v";
+      guc = "git add -u && git commit -v";
+      gaca = "git add . && git commit -v --amend";
+      gacan = "gaca --no-edit";
+  };
+
+  sessionVariables = {
+      EDITOR = "nvim";
+      LANG = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF-8";
+  };
+
+  initExtra = ''
+    # Linux alternative for OSX pbcopy
+    if ! [ -x "$(command -v pbpaste)" ]; then
+        alias pbpaste='xclip -selection clipboard -o'
+    fi
+
+    if ! [ -x "$(command -v pbcopy)" ]; then
+        alias pbcopy='xclip -selection clipboard'
+    fi
+  '';
+}
