5215d501b33ece23e7a519ee2f593e4b98753f66
Ernest Rekel <ernest.rekel@gmail.com>
2022-09-25 10:22:58 -0700

    Add vim-maximizer

--- nixpkgs/home-manager/modules/neovim/neovim.nix
+++ nixpkgs/home-manager/modules/neovim/neovim.nix
@@ -30,6 +30,7 @@ in {
         cmp-buffer
         (plugin "ray-x/go.nvim" "c75824b1f050c153ebfd5be65a318b9d4463d5a9")
         (plugin "ThePrimeagen/harpoon" "f4aff5bf9b512f5a85fe20eb1dcf4a87e512d971")
+        (plugin "szw/vim-maximizer" "2e54952fe91e140a2e69f35f22131219fcd9c5f1")
         cmp-calc
         cmp-nvim-lsp
         cmp-nvim-ultisnips
