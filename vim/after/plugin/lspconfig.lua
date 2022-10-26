99a1a2285ed9c68bf81b88053e85ddd42bcf3f94
Ernest Rekel <ernest.rekel@gmail.com>
2022-10-26 15:08:28 +0300

    Add indentation rules for ts and js

--- vim/after/plugin/lspconfig.lua
+++ vim/after/plugin/lspconfig.lua
@@ -1,8 +1,21 @@
 require'lspconfig'.intelephense.setup{}
 require'lspconfig'.jsonls.setup{}
 require'lspconfig'.pyright.setup{}
-require'lspconfig'.tsserver.setup{}
-require'lspconfig'.yamlls.setup{}
+require'lspconfig'.tsserver.setup{
+    settings = {
+        typescript = {
+            format = {
+                indentSize = 2,
+            }
+        },
+        javascript = {
+            format = {
+                indentSize = 2,
+            }
+        }
+    }
+}
+-- require'lspconfig'.yamlls.setup{}
 require'lspconfig'.terraformls.setup{}
 
 require'lspconfig'.gopls.setup{
