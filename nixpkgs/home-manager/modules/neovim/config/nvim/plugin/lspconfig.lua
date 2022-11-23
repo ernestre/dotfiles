75f67b5b6b3344136e6fa827217d8953946895ff
Ernest Rekel <ernest.rekel@gmail.com>
2022-11-23 21:57:11 +0200

    Use default golsp settings

--- nixpkgs/home-manager/modules/neovim/config/nvim/plugin/lspconfig.lua
+++ nixpkgs/home-manager/modules/neovim/config/nvim/plugin/lspconfig.lua
@@ -13,25 +13,8 @@ require 'lspconfig'.yamlls.setup {}
 require 'lspconfig'.terraformls.setup {}
 require 'lspconfig'.sumneko_lua.setup {}
 require 'lspconfig'.rnix.setup {}
-
-require 'lspconfig'.gopls.setup {
-    cmd = { "gopls" },
-    log_level = vim.lsp.protocol.MessageType.Log,
-    message_level = vim.lsp.protocol.MessageType.Log,
-    settings = {
-        gopls = {
-            analyses = {
-                fieldalignment = true,
-            },
-            ["build.experimentalWorkspaceModule"] = false,
-            ["formatting.gofumpt"] = true,
-            ["staticcheck"] = true,
-            ["ui.verboseOutput"] = true,
-        },
-    },
-}
-
-require 'lspconfig'.golangci_lint_ls.setup {}
+require 'lspconfig'.gopls.setup {}
+-- require 'lspconfig'.golangci_lint_ls.setup {}
 
 map('n', '<C-]>', '<Cmd>lua vim.lsp.buf.definition()<CR>')
 map('n', '<leader>ci', '<Cmd>lua vim.lsp.buf.references()<CR>')
