6559b087d7d9f5db3b6e4f3db5d9a6cef8287540
Ernest Rekel <ernest.rekel@gmail.com>
2023-06-01 09:13:23 +0300

    Add php dap configs

--- nixpkgs/home-manager/modules/neovim/config/nvim/after/plugin/dap.lua
+++ nixpkgs/home-manager/modules/neovim/config/nvim/after/plugin/dap.lua
@@ -24,3 +24,19 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
     dapui.close()
 end
+
+dap.adapters.php = {
+    type = "executable",
+    command = "node",
+    args = { "/tmp/vscode-php-debug/out/phpDebug.js" }
+}
+
+dap.configurations.php = {
+    {
+        type = "php",
+        request = "launch",
+        name = "Listen for Xdebug",
+        port = 9003,
+        log = true,
+    }
+}
