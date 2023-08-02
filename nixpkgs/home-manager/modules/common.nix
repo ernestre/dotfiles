c0ecefe1c23647ddf1aed9e35f2d08865e303e10
Ernest Rekel <ernest.rekel@gmail.com>
2023-08-02 08:12:51 +0300

    add lazy git

--- nixpkgs/home-manager/modules/common.nix
+++ nixpkgs/home-manager/modules/common.nix
@@ -66,6 +66,19 @@
     };
   };
 
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "delta --color-only --dark --paging=never";
+          useConfig = false;
+        };
+      };
+    };
+  };
+
   nixpkgs.overlays = [
     (import ../../overlays/markdown-preview-nvim { inherit pkgs; })
   ];
