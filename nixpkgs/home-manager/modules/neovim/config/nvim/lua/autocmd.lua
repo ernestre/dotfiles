2de02388e5554dd2250cac31824913bc339bf1be
Ernest Rekel <ernest.rekel@gmail.com>
2024-08-23 16:11:32 +0300

    Dont remove whitespaces in md files bcz they be important

--- nixpkgs/home-manager/modules/neovim/config/nvim/lua/autocmd.lua
+++ nixpkgs/home-manager/modules/neovim/config/nvim/lua/autocmd.lua
@@ -1,5 +1,21 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = '*', command = '%s/\\s\\+$//e'
+    pattern = '*',
+    callback = function()
+        local ft = vim.bo.filetype
+
+        if ft == "markdown" or ft == "vimwiki" then
+            return
+        end
+
+        -- Remove leading empty lines
+        vim.cmd([[%s/\%^\n//e]])
+        -- Remove trailing empty lines
+        vim.cmd([[%s/$\n\+\%$//e]])
+        -- Remove trailing spaces
+        vim.cmd([[%s/\s\+$//e]])
+        -- Remove trailing "\r"
+        vim.cmd([[%s/\r\+//e]])
+    end
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
