110eb75af85490c9f254b20bcd23b938af7647e8
Ernest Rekel <ernest.rekel@gmail.com>
2024-08-31 15:22:49 +0300

    Enable experimental features and enable gpg stuff

--- nixpkgs/nixos/thinkpad/configuration.nix
+++ nixpkgs/nixos/thinkpad/configuration.nix
@@ -11,6 +11,10 @@
       ./hardware-configuration.nix
     ];
 
+  nix = {
+    settings.experimental-features = "nix-command flakes";
+  };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -108,10 +112,10 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
